295c3c824752a460b774807662d58236c5cbc646
Alex Greenberg <acgreenberg2@gmail.com>
2024-04-13 14:11:28 -0400

    another change here

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
-# bundle install
-# # bundle exec rails assets:precompile
-# bundle exec rails assets:clean
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
